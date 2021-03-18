10b5481f1b2d5fa779bf271188895301c6397663
john <iplanetcn@gmail.com>
2021-03-18 14:01:45 +0800

    kotlin implementation.

---
+++ kotlin-solutions/src/test/kotlin/solutions/_004_MedianOfTwoSortedArraysTest.kt
@@ -0,0 +1,37 @@
+package solutions
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class _004_MedianOfTwoSortedArraysTest {
+    private var solutions: _004_MedianOfTwoSortedArrays? = null
+
+    companion object {
+        @JvmStatic
+        fun generateData(): Stream<Arguments> = Stream.of(
+                Arguments.of(intArrayOf(1, 3), intArrayOf(2), 2),
+                Arguments.of(intArrayOf(1, 2), intArrayOf(3, 4), 2.5)
+        )
+    }
+
+    @BeforeEach
+    fun setUp() {
+        solutions = _004_MedianOfTwoSortedArrays()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        solutions = null
+    }
+
+    @ParameterizedTest(name = "solution#{index} ==> input=\"{0} {1}\", expect={2}")
+    @MethodSource("generateData")
+    fun solution(num1: IntArray?, num2: IntArray?, answer: Double) {
+        Assertions.assertEquals(answer, solutions!!.findMedianSortedArrays(num1!!, num2!!))
+    }
+}
